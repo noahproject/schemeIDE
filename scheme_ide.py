53d6268247a85732359cea475199da594a9977d4
unknown <mlange806@gmail.com>
2015-01-30 11:12:46 -0600

    Cleaned up the code.

--- scheme_ide.py
+++ scheme_ide.py
@@ -1,30 +1,26 @@
 import tkinter as tk
 
-WELCOME_TEXT = "******************************************\n" \
-               "Welcome to Scheme IDE Beta Version!       \n" \
-               "Copyright 2014. All rights reserved.      \n" \
-               "                                          \n" \
-               "In this beta version, you can type on     \n" \
-               "the screen and nothing else. No refunds.  \n" \
-               "******************************************" 
+class SchemeIDE(tk.Frame):
+    '''
+    Scheme IDE
 
-class Application(tk.Frame):
-    def create_widgets(self, r):
-        editor = tk.Text(r,height=40,width=80,bg='black',fg='white', \
-                         insertbackground='blue')
-        editor.insert(tk.END, WELCOME_TEXT);
-        editor.mark_set(tk.INSERT, "1.0")
-        editor.pack()
-        editor.mark_set(tk.INSERT, "1.0")
+    This is the main application window.   
+    '''
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        master.minsize(width=333, height=333)
-        self.create_widgets(master)
+        master.minsize(width=300, height=300)
+        self.create_editor(master)
 
-root = tk.Tk()
-root.title('Scheme IDE Beta Version')
-app = Application(master=root)
-app.editor.mark_set(tk.INSERT, "1.0")
-app.mainloop()
-root.destroy()
+    def create_editor(self, r):
+        editor = tk.Text(r,height=30,width=60,bg='black',fg='white', \
+                         insertbackground='blue')
+        editor.pack()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Scheme IDE Beta Version')
+    app = SchemeIDE(master=root)
+    app.mainloop()
+    root.destroy()
