718fb3c66070ccf2f1ea1d2627b0b0a3b7a5ce4a
unknown <mlange@OAP-17.ttu.edu>
2015-02-03 11:50:00 -0600

    Added bare bones for a highlight text test.

--- test_scheme_ide.py
+++ test_scheme_ide.py
@@ -9,10 +9,20 @@ class TestSchemeIDE(unittest.TestCase):
         self.app = SchemeIDE(master=root)
 
     def test_run_code(self):
-        #Currenty fails because evaluator is not implemented.
+        #Currently fails because evaluator is not implemented.
         self.app.editor.insert('end', "(+ 2 2)")
         self.app.run_code()
         self.assertEqual('4', self.app.console.get('1.3', '1.4'))
+		
+class TestSchemeText(unittest.TestCase):
+	
+	def setUp(self):
+        root = tk.Tk()
+        self.editor = SchemeText(root)
+		
+	def test_highlight_pattern(self):
+		self.editor.highlight_pattern("lambda", "blue")
+		#TODO: Generate Popup asking if text appears to be blue.
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSchemeIDE)
