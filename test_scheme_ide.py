bda65ef7142eed0d4445e8802b934da78c3e4acc
Mark <mlange806@gmail.com>
2015-02-03 21:58:56 -0600

    Added test for keywork highlighting.

--- test_scheme_ide.py
+++ test_scheme_ide.py
@@ -1,5 +1,6 @@
 import unittest
 import tkinter as tk
+import tkinter.messagebox as MBox
 from scheme_ide import *
 
 class TestSchemeIDE(unittest.TestCase):
@@ -16,14 +17,34 @@ class TestSchemeIDE(unittest.TestCase):
 		
 class TestSchemeText(unittest.TestCase):
 	
-	def setUp(self):
-        root = tk.Tk()
-        self.editor = SchemeText(root)
+    def setUp(self):
+        self.root = tk.Tk()
+        self.editor = SchemeText(self.root)
+        self.gui_test_result = True
+
+    def _spawn(self):
+        '''
+        Asks the user to verify editor GUI functionality. Sets switch if a failure occurs.
+        At the end, destroys the editor stub which is in the mainloop by the time this 
+        method is called.
+        '''
+
+        result = MBox.askquestion('Test Highlight Pattern', 'Is lambda highlighted?')
+        if result == 'no': self.gui_test_result = False
+        self.root.destroy()
 		
-	def test_highlight_pattern(self):
-		self.editor.highlight_pattern("lambda", "blue")
-		#TODO: Generate Popup asking if text appears to be blue.
+    def test_highlight_pattern(self):
+        self.editor.insert('end', 'Look at this code: (+ (lambda other stuff)')
+        self.editor.highlight_pattern('lambda', 'red')
+        self.editor.after(1000, self._spawn)
+        self.editor.mainloop()
+        self.assertTrue(self.gui_test_result, 'Lambda is not highlighted.')
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestSchemeIDE)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestSchemeIDE)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestSchemeText)
+    full_suite = unittest.TestSuite([suite1, suite2])
+    #TODO: When tk.Tk() is started in suite1, it breaks suite2.
+    #unittest.TextTestRunner(verbosity=2).run(suite1)
+    unittest.TextTestRunner(verbosity=2).run(suite2)
+
